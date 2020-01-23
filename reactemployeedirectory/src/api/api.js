e9dbf118f734613b63bb17099750e68eb36876e1
Elliot Trejo <elliot.trejo.tcu@gmail.com>
2020-01-23 10:23:40 -0600

    Added yarn axios, created a new folder for the api and api.js. Referred to Jake's repo to link the API to the app. Will need to work with tutor to tie data together.

---
+++ reactemployeedirectory/src/api/api.js
@@ -0,0 +1,19 @@
+// Pulled from Jake O'Toole's repo
+
+
+const axios = require("axios")
+
+function Main(){
+
+    async function getRandomEmployees(){
+        try{
+            const response = await axios.get("https://randomuser.me/api/?results=5")
+            console.log(response.data)
+        }
+        catch(err){
+            console.log("error getting employees from api: ", err)
+        }
+    }
+    getRandomEmployees()
+}
+Main()
