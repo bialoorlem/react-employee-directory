fe548caaeca169250b535f23c6300f8d9b584a6c
Elliot Trejo <elliot.trejo.tcu@gmail.com>
2020-01-28 14:40:14 -0600

    moved files

---
+++ build/precache-manifest.45e309d43f4fd97b2bf3aa4330ebb823.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da3dd97230e9772540168412db8ffdec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64ae5ba7d268160e336e",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "242feeff6492e1f760c7",
+    "url": "/static/css/main.81f3b6ee.chunk.css"
+  },
+  {
+    "revision": "64ae5ba7d268160e336e",
+    "url": "/static/js/2.80be111b.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.80be111b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "242feeff6492e1f760c7",
+    "url": "/static/js/main.5a258d39.chunk.js"
+  },
+  {
+    "revision": "d71e86aaa6950959d626",
+    "url": "/static/js/runtime-main.7a54bfaa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
